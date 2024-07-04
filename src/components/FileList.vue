73456a67559ff9b00a354b5b9d3860525f8d0231
kotobukid <kotobuki@daqua.jp>
2024-07-04 16:11:12 +0900

    カレント画像の概念を導入

--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -4,17 +4,27 @@ import { invoke } from '@tauri-apps/api/tauri'
 import { convertFileSrc } from '@tauri-apps/api/tauri';
 
 const images = ref([]);
+const current = ref('');
 
 invoke('get_image_list', /* pass arguments */).then(imageList => {
   // use the imageList data
   images.value = imageList
 });
+
+const set_current = (i: string) => {
+    current.value = i;
+};
 </script>
 
 <template>
 <ul>
-    <li v-for="i in images">
-        <img class="face" :key="i" :src="`${convertFileSrc(i)}`">
+    <li v-for="i in images"
+        :class="current === i ? 'active': ''"
+        @click="set_current(i)"
+    >
+        <img
+         class="face" :key="i" :src="`${convertFileSrc(i)}`"
+        >
     </li>
 </ul>
 
@@ -25,8 +35,19 @@ ul {
     list-style: none;
 }
 
+li {
+    border: 3px solid grey;
+    padding: 5px;
+    margin: 5px;
+}
+
+li.active {
+    border-color: blue;
+    background-color: lightblue;
+}
+
 img.face {
-    width: 128px;
-    height: 128px;
+    width: 256px;
+    height: 256px;
 }
 </style>
