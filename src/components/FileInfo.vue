a329062dcdff8c63ace28b05ea924ad23928870a
kotobukid <kotobuki@daqua.jp>
2024-07-04 17:53:24 +0900

    データ永続化前まで

--- src/components/FileInfo.vue
+++ src/components/FileInfo.vue
@@ -1,43 +1,154 @@
 <script setup lang="ts">
 import {convertFileSrc} from "@tauri-apps/api/tauri";
+import {computed, inject} from "vue";
 
-const {file} = defineProps<{
+const props = defineProps<{
   file: string,
-  current: string
+  current: string,
+  disabled: boolean,
+  tags: Map<string, boolean>
 }>();
 
 const emits = defineEmits<{
   (e: 'set-current', value?: string): void
+  (e: 'clear-tags', value?: string): void
+  (e: 'set-disabled', value?: {file: string, disabled: boolean}): void
+  (e: 'set-tag', value?: {file: string, tag: string, active: boolean}): void
 }>();
 
-const set_current = (f: string) => {
-  emits('set-current', f);
+const available_tags = inject('available-tags');
+
+const set_current = () => {
+  emits('set-current', props.file);
+};
+
+const class_status = computed(() => {
+  if (props.disabled) {
+    return 'disabled';
+  } else {
+    return props.current === props.file ? 'active' : '';
+  }
+});
+
+const toggle_disabled = () => {
+  emits('set-disabled', {
+    disabled: !props.disabled,
+    file: props.file
+  });
+};
+
+// const tag_active = computed((tag: string): boolean => {
+//   return props.tags.has(tag) ? props.tags.get(tag) : false;
+// });
+
+const tag_active = (tag: string): boolean => {
+  return props.tags.has(tag) ? props.tags.get(tag) : false;
+};
+
+const toggle_tag_active = (tag: string) => {
+  emits('set-tag', {
+    file: props.file,
+    tag,
+    active: !props.tags.get(tag)
+  });
+};
+
+const clear_tags = () => {
+  emits('clear-tags', props.file);
 };
 </script>
 
 <template lang="pug">
-    .emote(
-        :class="current === file ? 'active': ''"
-        @click="set_current(file)"
-    )
-        img.face(:src="`${convertFileSrc(file)}`")
-        span {{ file }}
+  .emote(
+    :class="class_status"
+    @click="set_current"
+  )
+    .avatar
+      img.face(:src="`${convertFileSrc(props.file)}`")
+    .contents
+      // span {{ props.file }}
+      // br
+      button.toggle_disabled(@click.stop="toggle_disabled" :class="props.disabled ? 'current_disabled' : 'current_active'")
+        span(v-if="!props.disabled") 不使用にする
+        span(v-else="props.disabled") 使用可にする
+      hr
+      .tags
+        span.tag(
+          v-for="tag in available_tags"
+          )
+            br(v-if="tag[0] === 'sep'")
+            button.tag(
+              v-else
+              :class="tag_active(tag[0]) ? 'tag_active': ''"
+              @click.stop="toggle_tag_active(tag[0])"
+              v-text="tag[1]"
+            )
+      button.clear(@click.stop="clear_tags") クリア
 </template>
 
 <style scoped lang="less">
 .emote {
-    border: 3px solid grey;
-    padding: 5px;
-    margin: 5px;
-}
+  border: 3px solid grey;
+  padding: 5px;
+  margin: 5px;
+  display: flex;
 
-.emote.active {
+  &.disabled {
+    background-color: grey;
+    border-color: black;
+  }
+
+  &.active {
     border-color: blue;
     background-color: lightblue;
+  }
 }
 
+.avatar {
+  flex: 0 0 @image_width;
+}
+
+.contents {
+  margin-left: 10px;
+  flex: 1;
+}
+
+@image_width: 200px;
+
 img.face {
-    width: 256px;
-    height: 256px;
+  width: @image_width;
+  height: @image_width;
+}
+
+div.tags {
+  margin-bottom: 5px;
+}
+
+button.toggle_disabled {
+  &.current_disabled {
+    background-color: white;
+    color: black;
+  }
+  &.current_active {
+    background-color: grey;
+    color: white;
+  }
+}
+
+button.tag {
+  margin: 2px;
+  padding: 4px;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:active {
+    position: relative;
+    top: 1px;
+  }
+
+  &.tag_active {
+    background-color: #077607;
+    color: #d5ffd5;
+  }
 }
 </style>
--- src/components/FileList.vue
+++ src/components/FileList.vue
@@ -1,26 +1,117 @@
 <script setup lang="ts">
-import {ref} from "vue"
+import {ref, provide, type Ref} from "vue"
 import {invoke} from '@tauri-apps/api/tauri'
 import FileInfo from "./FileInfo.vue"
+import {appCacheDir} from "@tauri-apps/api/path";
 
-const images = ref([]);
+const images: Ref<FileInfo[]> = ref([]);
 const current = ref('');
 
+type FileInfo = {
+  file: string,
+  disabled: boolean,
+  tags: Map<EmoteKey, boolean>
+}
+
+
+const initialize_file_to_local = (file: string): FileInfo => {
+  return {
+    file,
+    disabled: false,
+    tags: new Map(emotionMap.entries())
+  };
+};
+
+type EmoteKey = string;
+type EmoteJp = string;
+type Emotion = [EmoteKey, EmoteJp]
+
+const available_tags: Emotion[] = [
+  ['neutral', '標準'],
+  ['smile', '笑顔'],
+  ['happy', '幸福'],
+  ['amazing', '称賛'],
+  ['curious', '興味'],
+  ['cheerful', '応援'],
+  ['active', '意欲'],
+  ['thinking', '考察'],
+  ['flatter', '媚態'],
+  ['sep', ''],
+  ['sad', '悲哀'],
+  ['angry', '怒り'],
+  ['bored', '退屈'],
+  ['annoying', '面倒'],
+  ['sleepy', '眠気'],
+  ['crying', '落涙'],
+  ['shame', '恥辱'],
+  ['shouting', '慟哭'],
+  ['despise', '軽蔑'],
+  ['disrespect', '失望'],
+];
+
+const emotionMap: Map<EmoteKey, boolean> = new Map();
+available_tags.forEach(tag => {
+  emotionMap.set(tag[0], false);
+});
+
+provide('available-tags', available_tags);
+
 invoke('get_image_list').then(imageList => {
-  images.value = imageList
+  images.value = imageList.map(initialize_file_to_local);
 });
 
 const set_current = (i: string) => {
   current.value = i;
 };
+
+const set_disabled = ({file, disabled}: { file: string, disabled: boolean }) => {
+  for (let i = 0; i < images.value.length; i++) {
+    const image = images.value[i];
+    if (image.file === file) {
+      image.disabled = disabled;
+      break;
+    }
+  }
+
+  images.value = [...images.value];
+};
+
+const set_tag = (info: {file: string, tag: EmoteKey, active: boolean}) => {
+  for (let i = 0; i < images.value.length; i++) {
+    const image = images.value[i];
+    if (image.file === info.file) {
+      image.tags.set(info.tag, info.active);
+      break;
+    }
+  }
+
+  images.value = [...images.value];
+};
+
+const clear_tags = (file: string) => {
+  for (let i = 0; i < images.value.length; i++) {
+    const image = images.value[i];
+    if (image.file === file) {
+      image.tags = new Map(emotionMap.entries());
+      break;
+    }
+  }
+
+  images.value = [...images.value];
+}
 </script>
 
 <template>
   <file-info
-    v-for="i in images"
-    :file="i"
+    v-for="image in images"
+    :file="image.file"
     :current="current"
+    :tags="image.tags"
+    :disabled="image.disabled"
     @set-current="set_current"
+    @set-disabled="set_disabled"
+    @set-tag="set_tag"
+    @clear-tags="clear_tags"
   />
 </template>
 
